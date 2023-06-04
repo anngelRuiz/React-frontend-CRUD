4ea928a2d4e0531c1346a42afa18b9ccfc4b9ea5
AnngelRuiz <anngelruiz@hotmail.com>
2023-06-03 23:42:20 -0600

    Simple Edit User and Delete User implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
   const [users, setUser] = useState([]);
+
+  const {id} = useParams();
 
   useEffect(() => {
     loadUsers();
   }, []);
 
   const loadUsers = async () => {
-    const result = await axios.get("http://localhost:8080/user");
+    const result = await axios.get("http://localhost:8080/users");
     setUser(result.data);
   };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/users/${id}`);
+    loadUsers();
+  }
 
   return (
     <div className="container">
@@ -41,8 +49,8 @@ export default function Home() {
                 <td>{user.deparment}</td>
                 <td>
                   <button className="btn btn-primary mx-2">View</button>
-                  <button className="btn btn-outline-primary mx-2">Edit</button>
-                  <button className="btn btn-danger mx-2">Delete</button>
+                  <Link className="btn btn-outline-primary mx-2" to={`/editUser/${user.id}`}>Edit</Link>
+                  <button className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</button>
                 </td>
               </tr>
             ))}
